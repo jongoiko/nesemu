31ab85e73b45170f5c493c7dd02e37ee545f06eb
Jon Goikoetxea <jon.goikoetxea.m@gmail.com>
2023-03-18 16:59:56 +0100

    PPU: Add PPUMASK register

--- src/nesemu/PPU.java
+++ src/nesemu/PPU.java
@@ -9,12 +9,14 @@ public class PPU extends MemoryMapped {
     private final byte patternMemory[][];
     private final byte paletteMemory[];
 
-    private PPUCTRL regPPUCTRL;
+    private final PPUCTRL regPPUCTRL;
+    private final PPUMASK regPPUMASK;
 
     public PPU() {
         patternMemory = new byte[PATTERN_TABLE_SIZE][2];
         paletteMemory = new byte[PALETTE_MEM_SIZE];
         regPPUCTRL = new PPUCTRL(0, 1, 0, 0, 8, true, false);
+        regPPUMASK = new PPUMASK(false, false, false, false, false, false, false, false);
     }
 
     private class PPUCTRL {
@@ -46,6 +48,41 @@ public class PPU extends MemoryMapped {
             spriteRowCount = (flags & 32) != 0 ? 16 : 8;
             isSlave = (flags & 64) == 0;
             generateNMIOnVBlank = (flags & 128) != 0;
+        }
+    }
+
+    private class PPUMASK {
+        public boolean grayscale;
+        public boolean showBackgroundLeft;
+        public boolean showSpritesLeft;
+        public boolean showBackground;
+        public boolean showSprites;
+        public boolean emphasizeRed;
+        public boolean emphasizeGreen;
+        public boolean emphasizeBlue;
+
+        public PPUMASK(boolean grayscale, boolean showBackgroundLeft,
+                boolean showSpritesLeft, boolean showBackground, boolean showSprites,
+                boolean emphasizeRed, boolean emphasizeGreen, boolean emphasizeBlue) {
+            this.grayscale = grayscale;
+            this.showBackgroundLeft = showBackgroundLeft;
+            this.showSpritesLeft = showSpritesLeft;
+            this.showBackground = showBackground;
+            this.showSprites = showSprites;
+            this.emphasizeRed = emphasizeRed;
+            this.emphasizeGreen = emphasizeGreen;
+            this.emphasizeBlue = emphasizeBlue;
+        }
+
+        public void update(byte flags) {
+            grayscale = (flags & 1) != 0;
+            showBackgroundLeft = (flags & 2) != 0;
+            showSpritesLeft = (flags & 4) != 0;
+            showBackground = (flags & 8) != 0;
+            showSprites = (flags & 16) != 0;
+            emphasizeRed = (flags & 32) != 0;
+            emphasizeGreen = (flags & 64) != 0;
+            emphasizeBlue = (flags & 128) != 0;
         }
     }
 
@@ -81,6 +118,9 @@ public class PPU extends MemoryMapped {
         switch (address & 7) {
             case 0:
                 regPPUCTRL.update(value);
+                break;
+            case 1:
+                regPPUMASK.update(value);
                 break;
             default:
                 throw new UnsupportedOperationException("Unsupported PPU register");
