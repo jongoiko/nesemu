db92af2ffcfa9d6ba38dfce3163f738777245440
Jon Goikoetxea <jon.goikoetxea.m@gmail.com>
2023-03-18 14:13:39 +0100

    PPU: Add PPUCTRL register

--- src/nesemu/PPU.java
+++ src/nesemu/PPU.java
@@ -2,10 +2,51 @@ package nesemu;
 
 public class PPU extends MemoryMapped {
     // TODO: OAM DMA
-    final private byte registers[];
+
+    private final static int PATTERN_TABLE_SIZE = 4096;
+    private final static int PALETTE_MEM_SIZE = 32;
+
+    private final byte patternMemory[][];
+    private final byte paletteMemory[];
+
+    private PPUCTRL regPPUCTRL;
 
     public PPU() {
-        this.registers = new byte[8];
+        patternMemory = new byte[PATTERN_TABLE_SIZE][2];
+        paletteMemory = new byte[PALETTE_MEM_SIZE];
+        regPPUCTRL = new PPUCTRL(0, 1, 0, 0, 8, true, false);
+    }
+
+    private class PPUCTRL {
+        public int baseNametableAddress;            // 0 to 3
+        public int nametableAddressIncrement;       // 1 or 32
+        public int spritePatternTableAddress;       // 0 or 1
+        public int backgroundPatternTableAddress;   // 0 or 1
+        public int spriteRowCount;                  // 8 or 16
+        public boolean isSlave;
+        public boolean generateNMIOnVBlank;
+
+        public PPUCTRL(int nametableAddress, int nametableAddressIncrement,
+                int spritePatternTableAddress, int backgroundPatternTableAddress,
+                int spriteRowCount, boolean isSlave, boolean generateNMIOnVBlank) {
+            this.baseNametableAddress = nametableAddress;
+            this.nametableAddressIncrement = nametableAddressIncrement;
+            this.spritePatternTableAddress = spritePatternTableAddress;
+            this.backgroundPatternTableAddress = backgroundPatternTableAddress;
+            this.spriteRowCount = spriteRowCount;
+            this.isSlave = isSlave;
+            this.generateNMIOnVBlank = generateNMIOnVBlank;
+        }
+
+        public void update(byte flags) {
+            baseNametableAddress = flags & 3;
+            nametableAddressIncrement = (flags & 4) != 0 ? 32 : 1;
+            spritePatternTableAddress = (flags & 8) >> 3;
+            backgroundPatternTableAddress = (flags & 16) >> 4;
+            spriteRowCount = (flags & 32) != 0 ? 16 : 8;
+            isSlave = (flags & 64) == 0;
+            generateNMIOnVBlank = (flags & 128) != 0;
+        }
     }
 
     private enum PPURegister {
@@ -32,11 +73,17 @@ public class PPU extends MemoryMapped {
 
     @Override
     byte readByteFromDevice(short address) {
-        return registers[address & 7];
+        return 0;
     }
 
     @Override
     void writeByteToDevice(short address, byte value) {
-        registers[address & 7] = value;
+        switch (address & 7) {
+            case 0:
+                regPPUCTRL.update(value);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unsupported PPU register");
+        }
     }
 }
