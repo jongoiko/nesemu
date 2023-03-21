801108d070d2efd14e283eb2f9035886d829eb69
Jon Goikoetxea <jon.goikoetxea.m@gmail.com>
2023-03-21 11:57:23 +0100

    PPU: Read palette colors and use attribute table

--- src/nesemu/PPU.java
+++ src/nesemu/PPU.java
@@ -7,6 +7,25 @@ public class PPU extends MemoryMapped {
 
     private final static int PALETTE_MEM_SIZE = 32;
     private final static int NAMETABLE_SIZE = 1024;
+    private final static int NAMETABLE_ATTRIBUTE_TABLE_INDEX = 960;
+    private final static Color[] SYSTEM_PALETTE = new Color[] {
+        new Color(84, 84, 84),    new Color(0, 30, 116),    new Color(8, 16, 144),    new Color(48, 0, 136),
+        new Color(68, 0, 100),    new Color(92, 0, 48),     new Color(84, 4, 0),      new Color(60, 24, 0),
+        new Color(32, 42, 0),     new Color(8, 58, 0),      new Color(0, 64, 0),      new Color(0, 60, 0),
+        new Color(0, 50, 60),     new Color(0, 0, 0),       new Color(0, 0, 0),       new Color(0, 0, 0),
+        new Color(152, 150, 152), new Color(8, 76, 196),    new Color(48, 50, 236),   new Color(92, 30, 228),
+        new Color(136, 20, 176),  new Color(160, 20, 100),  new Color(152, 34, 32),   new Color(120, 60, 0),
+        new Color(84, 90, 0),     new Color(40, 114, 0),    new Color(8, 124, 0),     new Color(0, 118, 40),
+        new Color(0, 102, 120),   new Color(0, 0, 0),       new Color(0, 0, 0),       new Color(0, 0, 0),
+        new Color(236, 238, 236), new Color(76, 154, 236),  new Color(120, 124, 236), new Color(176, 98, 236),
+        new Color(228, 84, 236),  new Color(236, 88, 180),  new Color(236, 106, 100), new Color(212, 136, 32),
+        new Color(160, 170, 0),   new Color(116, 196, 0),   new Color(76, 208, 32),   new Color(56, 204, 108),
+        new Color(56, 180, 204),  new Color(60, 60, 60),    new Color(0, 0, 0),       new Color(0, 0, 0),
+        new Color(236, 238, 236), new Color(168, 204, 236), new Color(188, 188, 236), new Color(212, 178, 236),
+        new Color(236, 174, 236), new Color(236, 174, 212), new Color(236, 180, 176), new Color(228, 196, 144),
+        new Color(204, 210, 120), new Color(180, 222, 120), new Color(168, 226, 144), new Color(152, 226, 180),
+        new Color(160, 214, 228), new Color(160, 162, 160), new Color(0, 0, 0),       new Color(0, 0, 0)
+    };
 
     private final byte paletteMemory[];
     private final byte nametableMemory[][];
@@ -36,16 +55,7 @@ public class PPU extends MemoryMapped {
 
     public void clockTick(Color[][] frameBuffer, CPU cpu, Cartridge cartridge) {
         if (scanline < 240 && column < 256) {
-            int tileNumber = nametableMemory[regPPUCTRL.baseNametableAddress][(scanline / 8) * 32 + column / 8];
-            int pixelAddress = (tileNumber * 16) + scanline % 8;
-            if (regPPUCTRL.backgroundPatternTableAddress == 1)
-                pixelAddress |= 0x1000;
-            int colorNum = ((cartridge.ppuReadByte((short)(pixelAddress)) >>> (7 - column % 8)) & 1)
-                        + 2 * ((cartridge.ppuReadByte((short)(pixelAddress + 8)) >>> (7 - column % 8)) & 1);
-            final Color[] colors = new Color[] {
-                Color.BLACK, Color.DARK_GRAY, Color.LIGHT_GRAY, Color.WHITE
-            };
-            frameBuffer[scanline][column] = colors[colorNum];
+            renderTile(frameBuffer, cartridge);
         }
         column++;
         if (column >= 341) {
@@ -166,6 +176,35 @@ public class PPU extends MemoryMapped {
                 twoByteValue = (short)(value << 8);
             upperByteSet = !upperByteSet;
         }
+    }
+
+    private void renderTile(Color[][] frameBuffer, Cartridge cartridge) {
+        int tileNumber = nametableMemory[regPPUCTRL.baseNametableAddress]
+                [(scanline / 8) * 32 + column / 8];
+        int pixelAddress = (tileNumber * 16) + scanline % 8;
+        if (regPPUCTRL.backgroundPatternTableAddress == 1)
+            pixelAddress |= 0x1000;
+        int lsb = (cartridge.ppuReadByte((short)(pixelAddress)) >>> (7 - column % 8)) & 1;
+        int msb = (cartridge.ppuReadByte((short)(pixelAddress + 8)) >>> (7 - column % 8)) & 1;
+        int colorNum = lsb + 2 * msb;
+        frameBuffer[scanline][column] =
+                SYSTEM_PALETTE[paletteMemory[getPaletteNumber() * 4 + colorNum]];
+    }
+
+    private int getPaletteNumber() {
+        int attributeAddress = NAMETABLE_ATTRIBUTE_TABLE_INDEX +
+                (scanline / 32) * 8 + column / 32;
+        byte attributeByte = nametableMemory[regPPUCTRL.baseNametableAddress]
+                [attributeAddress];
+        int regionNumberX = ((scanline / 16) * 16) % 2,
+                regionNumberY = (column / 16) % 2;
+        if (regionNumberX == 0 && regionNumberY == 0)
+            return attributeByte & 3;
+        else if (regionNumberX == 0 && regionNumberY == 1)
+            return (attributeByte >>> 2) & 3;
+        else if (regionNumberX == 1 && regionNumberY == 0)
+            return (attributeByte >>> 4) & 3;
+        return (attributeByte >>> 6) & 3;
     }
 
     private void writeByteAtPPUADDR(byte value) {
